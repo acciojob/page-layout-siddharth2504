0f63967654d189348385bdd07d90a2e86a6e6511
siddharth2504 <139268988+siddharth2504@users.noreply.github.com>
2025-04-22 23:06:53 +0530

    fifth

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
       {/* Do not remove the main div */}
       <PageLayout
         header={<h1>Welcome to my website</h1>}
-        footer={<h3>&copy; 2023 My Website.All rights reserved.</h3>}
+        footer={<p>&copy; 2023 My Website.All rights reserved.</p>}
       >
         <p>This is the content of my website.</p>
       </PageLayout>
